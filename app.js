5c1206a87aa1f40ec5dcb81c09e57f3db9751b72
Nick Sakovich <n.sakovich@gmail.com>
2017-06-16 10:39:47 +0300

    added api namespace

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
-app.use('/ping', require('./routes/ping'));
-app.use('/projects', require('./routes/projects'));
-app.use('/clusters', require('./routes/clusters'));
+app.use('/api/ping', require('./routes/ping'));
+app.use('/api/projects', require('./routes/projects'));
+app.use('/api/clusters', require('./routes/clusters'));
 app.use('/auth', require('./routes/auth'));
 
 // catch 404 and forward to error handler
